33afa8969e068a59fd14f9a0c9d5b75d098db836
Vadim <_s_e_t__@ukr.net>
2025-03-29 16:22:15 +0200

    add some code

--- redux/postsSlice.ts
+++ redux/postsSlice.ts
@@ -2,6 +2,8 @@ import { Post } from "@/types/post";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
+import { fetchPosts } from "./operations";
+
 interface PostState {
   posts: Post[];
   isLoading: boolean;
@@ -18,8 +20,22 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state, actiion) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action: PayloadAction<Post[]>) => {
+        state.isLoading = false;
+        state.posts = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || "Error loading posts";
+      });
+  },
 });
 
-export const { } = postsSlice.actions;
-export const postsReducer = postsSlice.reducer 
+export const {} = postsSlice.actions;
+export const postsReducer = postsSlice.reducer;
